61f379a8d10c1241612d61757c6a6ed0b28f3466
fariasmk <fariasmk@hotmail.com>
2022-01-19 19:44:51 -0300

    ver. 01

---
+++ src/main/java/com/maikon/os/repositories/TecnicoRepository.java
@@ -0,0 +1,11 @@
+package com.maikon.os.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.maikon.os.domain.Tecnico;
+
+@Repository
+public interface TecnicoRepository extends JpaRepository<Tecnico, Integer> {
+
+}
---
+++ src/main/java/com/maikon/os/services/DBService.java
@@ -0,0 +1,41 @@
+package com.maikon.os.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maikon.os.domain.Cliente;
+import com.maikon.os.domain.OS;
+import com.maikon.os.domain.Tecnico;
+import com.maikon.os.domain.enums.Prioridade;
+import com.maikon.os.domain.enums.Status;
+import com.maikon.os.repositories.ClienteRepository;
+import com.maikon.os.repositories.OSRepository;
+import com.maikon.os.repositories.TecnicoRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private OSRepository osRepository;
+	
+	public void instanciaDB() {
+		Tecnico t1 = new Tecnico(null, "Maikon Farias", "409.983.520-04", "(88) 88888-8888");
+		Cliente c1 = new Cliente(null, "Betina Campos", "171.526.320-06", "(88) 98888-7777");
+
+		OS os1 = new OS(null, Prioridade.ALTA, "Teste create OD", Status.ANDAMENTO, t1, c1);
+
+		t1.getList().add(os1);
+		c1.getList().add(os1);
+
+		tecnicoRepository.saveAll(Arrays.asList(t1));
+		clienteRepository.saveAll(Arrays.asList(c1));
+		osRepository.saveAll(Arrays.asList(os1));
+	}
+
+}
