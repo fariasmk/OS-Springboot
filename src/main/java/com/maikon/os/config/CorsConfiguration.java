f50f26dcf7c4339bbc4708324729da29d6406d7f
fariasmk <fariasmk@hotmail.com>
2022-02-25 01:43:11 -0300

    Liberação Cors 2

--- src/main/java/com/maikon/os/config/CorsConfiguration.java
+++ src/main/java/com/maikon/os/config/CorsConfiguration.java
@@ -10,6 +10,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
