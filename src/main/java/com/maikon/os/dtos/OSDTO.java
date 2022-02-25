4ae8d0bebc1573de943e6f056623012b091d8c9c
fariasmk <fariasmk@hotmail.com>
2022-02-25 11:22:49 -0300

    Atualização OS

--- src/main/java/com/maikon/os/dtos/OSDTO.java
+++ src/main/java/com/maikon/os/dtos/OSDTO.java
@@ -1,111 +1,114 @@
 package com.maikon.os.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.maikon.os.domain.OS;
+import com.maikon.os.domain.enums.Prioridade;
+import com.maikon.os.domain.enums.Status;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-import org.hibernate.validator.constraints.NotEmpty;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.maikon.os.domain.OS;
-
 @SuppressWarnings("deprecation")
 public class OSDTO implements Serializable {
-	private static final long serialVersionUID = 1L;
+    @Serial
+    private static final long serialVersionUID = 1L;
 
-	private Integer id;
+    private Integer id;
 
-	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
-	private LocalDateTime dataAbertura;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private LocalDateTime dataAbertura;
 
-	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
-	private LocalDateTime dataFechamento;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private LocalDateTime dataFechamento;
 
-	private Integer prioridade;
+    private Integer prioridade;
 
-	@NotEmpty(message = "O campo de OBSERVAÇÕES é requerido")
-	private String observacoes;
-	private Integer status;
-	private Integer tecnico;
-	private Integer cliente;
+    @NotEmpty(message = "O campo de OBSERVAÇÕES é requerido")
+    private String observacoes;
+    private Integer status;
+    private Integer tecnico;
+    private Integer cliente;
 
-	public OSDTO() {
-		super();
-	}
+    public OSDTO() {
+        super();
+    }
 
-	public OSDTO(OS obj) {
-		super();
-		this.id = obj.getId();
-		this.dataAbertura = obj.getDataAbertura();
-		this.dataFechamento = obj.getDataFechamento();
-		this.prioridade = obj.getPrioridade().getCod();
-		this.observacoes = obj.getObservacoes();
-		this.status = obj.getStatus().getCod();
-		this.tecnico = obj.getTecnico().getId();
-		this.cliente = obj.getCliente().getId();
-	}
+    public OSDTO(OS obj) {
+        super();
+        this.id = obj.getId();
+        this.dataAbertura = obj.getDataAbertura();
+        this.dataFechamento = obj.getDataFechamento();
+        this.prioridade = obj.getPrioridade().getCod();
+        this.observacoes = obj.getObservacoes();
+        this.status = obj.getStatus().getCod();
+        this.tecnico = obj.getTecnico().getId();
+        this.cliente = obj.getCliente().getId();
+    }
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public LocalDateTime getDataAbertura() {
-		return dataAbertura;
-	}
+    public LocalDateTime getDataAbertura() {
+        return dataAbertura;
+    }
 
-	public void setDataAbertura(LocalDateTime dataAbertura) {
-		this.dataAbertura = dataAbertura;
-	}
+    public void setDataAbertura(LocalDateTime dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
 
-	public LocalDateTime getDataFechamento() {
-		return dataFechamento;
-	}
+    public LocalDateTime getDataFechamento() {
+        return dataFechamento;
+    }
 
-	public void setDataFechamento(LocalDateTime dataFechamento) {
-		this.dataFechamento = dataFechamento;
-	}
+    public void setDataFechamento(LocalDateTime dataFechamento) {
+        this.dataFechamento = dataFechamento;
+    }
 
-	public Integer getPrioridade() {
-		return prioridade;
-	}
+    public Prioridade getPrioridade() {
+        return Prioridade.toEnum(this.prioridade);
+    }
 
-	public void setPrioridade(Integer prioridade) {
-		this.prioridade = prioridade;
-	}
+    public void setPrioridade(Integer prioridade) {
+        this.prioridade = prioridade;
+    }
 
-	public String getObservacoes() {
-		return observacoes;
-	}
+    public String getObservacoes() {
+        return observacoes;
+    }
 
-	public void setObservacoes(String observacoes) {
-		this.observacoes = observacoes;
-	}
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
 
-	public Integer getStatus() {
-		return status;
-	}
+    public Status getStatus() {
+        return Status.toEnum(this.status);
+    }
 
-	public void setStatus(Integer status) {
-		this.status = status;
-	}
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
 
-	public Integer getTecnico() {
-		return tecnico;
-	}
+    public Integer getTecnico() {
+        return tecnico;
+    }
 
-	public void setTecnico(Integer tecnico) {
-		this.tecnico = tecnico;
-	}
+    public void setTecnico(Integer tecnico) {
+        this.tecnico = tecnico;
+    }
 
-	public Integer getCliente() {
-		return cliente;
-	}
+    public Integer getCliente() {
+        return cliente;
+    }
 
-	public void setCliente(Integer cliente) {
-		this.cliente = cliente;
-	}
+    public void setCliente(Integer cliente) {
+        this.cliente = cliente;
+    }
 
 }
